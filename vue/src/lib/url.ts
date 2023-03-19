d32360cc90a47562676ca3b03328edb4dcd1d378
Caesar525 <yeng525@hotmail.com>
2023-03-19 17:24:55 +0800

    添加部署

--- vue/src/lib/url.ts
+++ vue/src/lib/url.ts
@@ -1,2 +1,3 @@
-const URL = process.env.NODE_ENV === 'production' ? 'https://yourdomain/' : 'https://localhost:44311/';
+const URL = process.env.NODE_ENV === 'production' ? 'http://localhost:8091/' : 'http://localhost:44311/';
+//const URL = process.env.NODE_ENV === 'production' ? 'https://yourdomain/' : 'https://localhost:8091/';
 export default URL;
