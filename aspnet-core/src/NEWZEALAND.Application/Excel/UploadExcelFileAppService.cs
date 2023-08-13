a75c6727886e865b18731ecb9b0c3fe519ffa83a
Caesar525 <yeng525@hotmail.com>
2023-08-13 13:21:00 +0800

    增加后台方法

---
+++ aspnet-core/src/NEWZEALAND.Application/Excel/UploadExcelFileAppService.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEWZEALAND.Excel
+{
+    public class UploadExcelFileAppService : NEWZEALANDAppServiceBase
+    {
+        public UploadExcelFileAppService()
+        {
+
+        }
+        //ExcelAppService.cs
+        /// <summary>
+        /// 接收上传文件方法
+        /// </summary>
+        /// <param name="file">文件内容</param>
+        /// <returns>文件名称</returns>
+        public async Task<string> UploadFile(IFormFile file)
+        {
+            //FileDir是存储临时文件的目录，相对路径
+            string FileDir = "/File/ExcelTemp";
+            string url = await WriteFile(file, FileDir);
+
+            string fullpath = Path.GetFullPath($"{Environment.CurrentDirectory}" + url);
+
+            return Path.GetFileName(url);
+        }
+        /// <summary>
+        /// 写入文件
+        /// </summary>
+        /// <param name="avatar"></param>
+        /// <param name="reDir"></param>
+        /// <returns></returns>
+        public async Task<string> WriteFile(IFormFile avatar, string reDir)
+        {
+            string reName = Guid.NewGuid() + Path.GetExtension(avatar.FileName);
+            string dir = await this.GetDirPath(reDir);
+            string path = $"{dir}\\{reName}";
+            Stream stream = avatar.OpenReadStream();
+            using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            {
+                await avatar.CopyToAsync(fileStream);
+            }
+            return $"{reDir}/{reName}";
+        }
+        public Task<string> GetDirPath(string reDir)
+        {
+            string dir = $"{Environment.CurrentDirectory}/{reDir}";
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            return Task.FromResult(Path.GetFullPath(dir));
+        }
+
+        ///// <summary>
+        ///// 导入
+        ///// </summary>
+        ///// <param name="input">导入excel参数</param>
+        ///// <returns></returns>
+        //[HttpPost]
+        //public async Task ImportExcel(XXXImprotExcelInput input)
+        //{
+        //    var data = await excelAppService.GetData<XXXImportExcelDto>(input.FileName);
+        //    if (!data.Any())
+        //    {
+        //        return;
+        //    }
+        //    //你的逻辑
+        //}
+
+        ////XXXImprotExcelInput.cs
+        ///// <summary>
+        ///// 导入excel
+        ///// </summary>
+        //public class XXXImprotExcelInput
+        //{
+        //    /// <summary>
+        //    /// 上传的excel文件名称
+        //    /// </summary>
+        //    public string FileName { get; set; }
+        //    //你的其他参数
+        //}
+        ////ExcelAppService.cs
+        ///// <summary>
+        ///// 解析excel数据
+        ///// </summary>
+        ///// <typeparam name="T">要解析的数据类型</typeparam>
+        ///// <param name="fileName">excel文件名称，不含路径</param>
+        ///// <returns></returns>
+        //internal async Task<IEnumerable<T>> GetData<T>(string fileName) where T : class, new()
+        //{
+        //    var fullpath = GetFullPath(fileName);
+        //    var result = await importer.Import<T>(fullpath);
+        //    if (result.HasError)
+        //    {
+        //        var errFile = Path.GetFileNameWithoutExtension(fileName) + "_" + Path.GetExtension(fileName);
+        //        //如果excel文件内容不符合要求（格式错误、必填数据未填、数据类型错误），则弹出错误提示并给出下载链接
+        //        throw new UserFriendlyException("导入错误", GetErrorExcelDownLoadUrl(errFile));
+        //    }
+        //    return result.Data;
+        //}
+        ///// <summary>
+        ///// 下载excel文件
+        ///// </summary>
+        ///// <param name="fileName"></param>
+        ///// <returns></returns>
+        //[HttpGet]
+        //public async Task<FileContentResult> DownLoadFile(string fileName)
+        //{
+        //    var fullPath = GetFullPath(fileName);
+        //    byte[] fileBytes = await File.ReadAllBytesAsync(fullPath);
+        //    return new FileContentResult(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet)
+        //    {
+        //        FileDownloadName = fileName
+        //    };
+        //}
+        ///// <summary>
+        ///// 获取文件全路径
+        ///// </summary>
+        ///// <param name="fileName"></param>
+        ///// <returns></returns>
+        //private string GetFullPath(string fileName)
+        //{
+        //    fileName = Path.GetFileName(fileName);
+        //    var fullpath = Path.GetFullPath(Environment.CurrentDirectory.EnsureEndsWith('/') + FileDir.EnsureEndsWith('/') + fileName);
+        //    return fullpath;
+        //}
+        ///// <summary>
+        ///// 获取excel下载链接
+        ///// </summary>
+        ///// <param name="fileName"></param>
+        ///// <returns></returns>
+        //private string GetErrorExcelDownLoadUrl(string fileName)
+        //{
+        //    return $"请按照excel文件内的错误提示修改后再次导入，<a href='{GetHost()}/api/services/app/Excel/DownLoadFile?fileName={fileName}' target='_blank'>点击下载excel</a>"
+        //        ;
+        //}
+        ///// <summary>
+        ///// 获取当前域名地址
+        ///// </summary>
+        ///// <returns></returns>
+        //private string GetHost()
+        //{
+        //    var req = httpContextAccessor.HttpContext.Request;
+        //    return $"{req.Scheme}://{req.Host}";
+        //}
+    }
+}
--- vue/src/views/setting/consumelist/upload-consumelist.vue
+++ vue/src/views/setting/consumelist/upload-consumelist.vue
@@ -1,62 +1,80 @@
 <template>
     <div>
-        <Modal :title="L('EditConsumeList')"
-               :value="value"
-               @on-ok="save"
-               @on-visible-change="visibleChange">
-            <form ref="consumelistForm" label-position="top" :rules="consumelistRule" :model="consumelist">
-                <Tabs value="detial">
-                  <TabPane :label="L('ConsumeDetails')" name="detail"></TabPane>
-                </Tabs>
-            </form>
+        <Modal :title="L('EditConsumeList')" >
+            <Tabs value="detial">
+                <TabPane :label="L('ConsumeDetails')" name="detail">
+                  <!-- 上传控件 -->
+                  <div>
+                      <Upload
+                        :action="uploadURL"
+                        :on-success="onSuccess"
+                        accept=".xls, .xlsx"
+                        :show-upload-list="false"
+                      >
+                        <Button icon="android-add" type="primary">{{btnTitle}}</Button>
+                      </Upload>
+                    </div>
+                </TabPane>
+              </Tabs>
         </Modal>
     </div>
 </template>
 <script lang="ts">
     import { Component, Vue,Inject, Prop,Watch } from 'vue-property-decorator';
-    import Util from '../../../lib/util'
-    import AbpBase from '../../../lib/abpbase'
+    import Util from '../../../lib/util';
+    import AbpBase from '../../../lib/abpbase';
     import ConsumeList from '@/store/entities/ConsumeList';
+    import appconst from "../../../lib/appconst";
+
     @Component
     export default class UploadConsumeList extends AbpBase{
-        @Prop({type:Boolean,default:false}) value:boolean;
-        consumelist: ConsumeList = new ConsumeList();
-        get permissions(){
-            return this.$store.state.role.permissions
+        uploadURL =
+        appconst.remoteServiceBaseUrl + "/api/services/app/Excel/UploadExcelFile";
+        async onSuccess(response, file, fileList) {
+            //上传完成触发事件uploadCompleted
+            this.$emit("uploadCompleted", response.result);
         }
-        save(){
-            (this.$refs.consumelistForm as any).validate(async (valid:boolean)=>{
-                if (valid) {
-                    this.consumelist.consumemonth = new Date(this.consumelist.consumemonth.setDate(this.consumelist.consumemonth.getDate() + 1));
-                    this.consumelist.happentime = new Date(this.consumelist.happentime.setDate(this.consumelist.happentime.getDate() + 1));
-                    await this.$store.dispatch({
-                        type:'consumelist/update',
-                        data: this.consumelist
-                    });
-                    (this.$refs.consumelistForm as any).resetFields();
-                    this.$emit('save-success');
-                    this.$emit('input',false);
-                }
-            })
-        }
-        cancel(){
-            (this.$refs.consumelistForm as any).resetFields();
-            this.$emit('input',false);
-        }
-        visibleChange(value:boolean){
-            if(!value){
-                this.$emit('input',value);
-            }else{
-                this.consumelist = Util.extend(true, {}, this.$store.state.consumelist.editConsumeList);
-            }
-        }
-        consumelistRule = {
-            consumemonth: [],
-            consume: [],
-            usage: [],
-            remark: [],
-            location: [],
-            happentime: []
-        }
+        /**按钮显示内容 */
+        @Prop({ type: String, default: "导入" }) btnTitle: String;
+
+        //@Prop({type:Boolean,default:false}) value:boolean;
+        // consumelist: ConsumeList = new ConsumeList();
+        // get permissions(){
+        //     return this.$store.state.role.permissions
+        // }
+        // save(){
+        //     (this.$refs.consumelistForm as any).validate(async (valid:boolean)=>{
+        //         if (valid) {
+        //             this.consumelist.consumemonth = new Date(this.consumelist.consumemonth.setDate(this.consumelist.consumemonth.getDate() + 1));
+        //             this.consumelist.happentime = new Date(this.consumelist.happentime.setDate(this.consumelist.happentime.getDate() + 1));
+        //             await this.$store.dispatch({
+        //                 type:'consumelist/update',
+        //                 data: this.consumelist
+        //             });
+        //             (this.$refs.consumelistForm as any).resetFields();
+        //             this.$emit('save-success');
+        //             this.$emit('input',false);
+        //         }
+        //     })
+        // }
+        // cancel(){
+        //     (this.$refs.consumelistForm as any).resetFields();
+        //     this.$emit('input',false);
+        // }
+        // visibleChange(value:boolean){
+        //     if(!value){
+        //         this.$emit('input',value);
+        //     }else{
+        //         this.consumelist = Util.extend(true, {}, this.$store.state.consumelist.editConsumeList);
+        //     }
+        // }
+        // consumelistRule = {
+        //     consumemonth: [],
+        //     consume: [],
+        //     usage: [],
+        //     remark: [],
+        //     location: [],
+        //     happentime: []
+        // }
     }
 </script>
